6b535e7e45e1c3d89817fcbbb8b0f3d049c947e1
Ai_Ma <33986466+manswan16@users.noreply.github.com>
2023-11-13 23:43:18 -0600

    Add files via upload
    String, ok

---
+++ StringManipulation1.java
@@ -0,0 +1,49 @@
+package Day7;
+
+public class StringManipulation1 {
+    public static void main(String[] args) {
+
+//        Declare this variable:
+//
+
+        String message = "Hello everyone, Let's do Java String exercises";
+
+//        So you must have 5 System.out.println();
+//1. print out the character 'y' from message using charAt() method
+
+        System.out.println(message.charAt(10));
+
+//2. print out the length of the String message
+
+        System.out.println(message.length());
+
+//3. print out the String message all in uppercase
+
+        System.out.println(message.toUpperCase());
+
+//4. print out the String message all in lowercase
+
+        System.out.println(message.toLowerCase());
+
+//5. print only letter with index 5
+
+        System.out.println(message.charAt(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
