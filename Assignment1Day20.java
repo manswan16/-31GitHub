23b3e000e4f4282248ccebf7fa6606292c10d803
Ai_Ma <33986466+manswan16@users.noreply.github.com>
2023-11-13 23:40:09 -0600

    Add files via upload
    first file

---
+++ Assignment1Day20.java
@@ -0,0 +1,115 @@
+package Day20;
+
+import java.util.Scanner;
+
+public class Assignment1Day20 {
+    public static void main(String[] args) {
+
+
+//1.  Write a program which prints the text below using nested loops.
+//Output:
+//***
+//****
+//*****
+//******
+//*******
+//
+//        for (int i = 3; i <= 7; i++){
+//            for (int j = 1; j<=i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
+
+
+//2: Easy
+//Write a program which prints the rectangle below using nested loops.
+//First you need to enter some int number using Scanner.
+//Input: 8
+//Output:
+//88888888
+//88888888
+//88888888
+//88888888
+//88888888
+//88888888
+//88888888
+//88888888
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter number");
+//        int a = sc.nextInt();
+//
+//        for (int i = 1; i<= 8; i++){
+//            for (int j = 1; j<=8; j++){
+//                System.out.print(a);
+//            }
+//            System.out.println();
+//        }
+
+
+
+
+//3: Easy
+//Write a program where you need to create this 2D array
+//int[][] array2D = new int[2][3].
+//1) print your 2D array using nested loops, make it like a table.
+
+//        int [][] marks = {  {2,6,12,10},
+//                            {1,5,3,15},
+//                            {8,9,16,4} };
+//
+//        for (int row = 0; row < marks.length; row++) {
+//            for (int column = 0; column <marks[0].length; column++) {
+//                System.out.print(marks [2][3]+" ");
+//            }
+//            System.out.println();
+//            }
+
+
+
+
+//4:Difficult
+//Write a program where you need to declare this 2D array
+//String[][] names2D = new String[2][3].
+//1) using nested for loop and Scanner assign values to element in 2D array.
+//2) print your 2D array  using nested loops, make it like a table.                                                                                     3) print your 2D array in reverse order using nested loops, make it like a table.
+//Input: Ethan
+//Input: Benjamin
+//Input: Alexander
+//Input: Isabella
+//Input: Henry
+//Input: Olivia
+//Output:
+//Norm
+//Olivia Henry Isabella
+//Alexander Benjamin Ethan
+
+        //String[][] names2D = new String[2][3].
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 6 names");
+        String names = sc.nextLine();
+        
+//        String [][] names2D = new String[2][3];
+//        for (int i = 0; i < ; i++) {
+//
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
