11934d1ac4bb6014a2f2dde92c90adafdc22a837
Ai_Ma <33986466+manswan16@users.noreply.github.com>
2023-11-20 04:22:51 -0600

    Day7of31
    ok

---
+++ Palindromes.java
@@ -0,0 +1,28 @@
+public class Palindromes {
+    public static void main(String[] args) {
+
+        String word = "level";
+        String reverse = "";
+        for (int i =word.length()-1; i>=0; i--) {
+            reverse += word.charAt(i);
+        }
+
+        System.out.println(reverse);
+
+
+        boolean palindrome = true;
+        for (int i = 0; i < word.length()-1; i++) {
+            if (word.charAt(i)!= reverse.charAt(i)){
+                palindrome= false;
+            }
+        }
+
+        if (palindrome) {
+            System.out.println("Palindrome!");
+        }
+        else {
+            System.out.println("Not a palindrome");
+        }
+
+    }
+}
