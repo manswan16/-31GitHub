37dfc20a4a5cd3818d2fe935a7a24c031bc1ae42
Ai_Ma <33986466+manswan16@users.noreply.github.com>
2023-11-22 07:06:44 -0600

    Day9of31
    ok

---
+++ ForLoops.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class ForLoops {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <=5; i++) {
+            System.out.println("GROWTH HUNGRY CHALLENGE");
+        }
+        for (int i = 0; i <= 31; i++) {
+            System.out.println(i);
+        }
+        int [] days = {7, 14, 21};
+        for (int i = 0; i < days.length; i++) {
+            System.out.println(days [i]);
+        }
+
+
+
+    }
+}
