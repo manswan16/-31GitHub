d5234ef2f347644da06c9418ef4ff896f95667d9
Ai_Ma <33986466+manswan16@users.noreply.github.com>
2023-11-23 03:36:56 -0600

    Day10/31
    oki

---
+++ EscapeSequences.java
@@ -0,0 +1,16 @@
+public class EscapeSequences {
+    public static void main(String[] args) {
+
+        System.out.println("The \"fish\" was blue");
+        System.out.println("These are double quotes \" \"");
+
+        System.out.println("This is a backslash \\");
+        System.out.println("This file is located at ~\\Desktop\\JavaProjects");
+
+        System.out.println("This is a tab \t!!!");
+        System.out.println("This \n is on \n multiple \n lines");
+
+
+    }
+
+}
