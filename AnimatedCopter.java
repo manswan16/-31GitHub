febfc1c542dc361c02b0cfa1ee25b9df337107df
Ai_Ma <33986466+manswan16@users.noreply.github.com>
2023-11-24 00:18:59 -0600

    Day11of31
    good

---
+++ AnimatedCopter.java
@@ -0,0 +1,72 @@
+package gitChallenge31;
+
+public class AnimatedCopter {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        while(true) {
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+
+
+            System.out.println("      <<<<:<<<<:LOL:        ");
+            System.out.println("                 A                    ");
+            System.out.println("         /---------------             ");
+            System.out.println("  LOL====            [ ] \\           ");
+            System.out.println("   L     \\                \\         ");
+            System.out.println("          \\                \\         ");
+            System.out.println("           \\_______________]          ");
+            System.out.println("               I       I               ");
+            System.out.println("         -----------------------/         ");
+            System.out.println("Aichu's copter");
+
+            Thread.sleep(400);
+
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+
+
+            System.out.println("               :LOL:>>>>:>>>>        ");
+            System.out.println("                 A                    ");
+            System.out.println("   L     /---------------             ");
+            System.out.println("  LOL====            [ ] \\           ");
+            System.out.println("         \\                \\         ");
+            System.out.println("          \\                \\         ");
+            System.out.println("           \\_______________]          ");
+            System.out.println("               I       I               ");
+            System.out.println("         -----------------------/         ");
+            System.out.println("Aichu's copter");
+
+
+            Thread.sleep(400);
+
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+            System.out.println("                  ");
+
+
+
+        }
+
+    }
+}
