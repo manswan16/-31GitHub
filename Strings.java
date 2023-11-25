9d70c6541802e59a2678f3270fb405393b96d68c
Ai_Ma <33986466+manswan16@users.noreply.github.com>
2023-11-25 02:35:06 -0600

    Day12of31
    good

---
+++ Strings.java
@@ -0,0 +1,16 @@
+package gitChallenge31;
+
+public class Strings {
+    public static void main(String[] args) {
+
+        System.out.println("My ____ sister accidentally ___ all the ___");
+
+
+        String adjective = "lovely";
+        String verb = "ate";
+        String noun = "ice cream";
+
+        System.out.println("My " + adjective + " sister accidentally " + verb + " all the " + noun);
+
+    }
+}
