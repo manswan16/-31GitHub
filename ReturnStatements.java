7febd2d5c00b2f5fb91077aa668e7b1ee4358131
Ai_Ma <33986466+manswan16@users.noreply.github.com>
2023-12-01 03:23:44 -0600

    Day18of31
    ok

---
+++ ReturnStatements.java
@@ -0,0 +1,28 @@
+package gitChallenge31;
+
+public class ReturnStatements {
+
+    public static void main(String[] args) {
+        String shouting = caps("why are you reading my diary mom?!");
+        System.out.println(shouting);
+
+        int[] awesomeArray = gimmeArrayFromInts(3, 7, 1);
+        System.out.println(awesomeArray[0]);
+        System.out.println(awesomeArray[1]);
+        System.out.println(awesomeArray[2]);
+    }
+
+    public static String caps(String s) {
+        return s.toUpperCase();
+    }
+
+    public static int[] gimmeArrayFromInts(int a, int b, int c) {
+        int[] array = new int[3];
+        array[0] = a;
+        array[1] = b;
+        array[2] = c;
+        return array;
+    }
+
+
+}
